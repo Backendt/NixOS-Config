b42ed3ec31c434f496b83be21e08410b413786f7
Backendt <matisse.beckandt@proton.me>
2024-09-03 12:38:06 +0200

    Set primary monitor on XWayland

--- modules/user/hyprland/desktop-monitors.nix
+++ modules/user/hyprland/desktop-monitors.nix
@@ -1,9 +1,16 @@
 { config, pkgs, ... }:
 
 {
-    wayland.windowManager.hyprland.settings.monitor = [
-        "DP-1, 1920x1080@144, 0x0, 1"
-        "HDMI-A-1, 1920x1080, 1920x-400, 1, transform, 1"
-        "Unknown-1, disable"
-    ];
+    wayland.windowManager.hyprland.settings = {
+        monitor = [
+            "DP-1, 1920x1080@144, 0x0, 1"
+            "HDMI-A-1, 1920x1080, 1920x-400, 1, transform, 1"
+            "Unknown-1, disable"
+        ];
+
+        # Set primary monitor on XWayland
+        exec = "xrandr --output 'DP-1' --primary";
+    };
+
+    home.packages = [ pkgs.xorg.xrandr ];
 }
