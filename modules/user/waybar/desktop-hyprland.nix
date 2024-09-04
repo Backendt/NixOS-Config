930f39c3d8c0705cef77f5390b9ee432f7607183
Backendt <matisse.beckandt@proton.me>
2024-09-04 19:32:56 +0200

    Fixed Waybar CPU temperature display on desktop

--- modules/user/waybar/desktop-hyprland.nix
+++ modules/user/waybar/desktop-hyprland.nix
@@ -3,5 +3,14 @@
 {
     imports = [ ./common-hyprland.nix ];
 
-    programs.waybar.settings.main.output = "DP-1";
+    programs.waybar.settings.main = {
+        # Only display waybar on main monitor
+        output = "DP-1";
+
+        # Change the temperature zone used for CPU
+        temperature.thermal-zone = 2;
+    };
+
+
+
 }
