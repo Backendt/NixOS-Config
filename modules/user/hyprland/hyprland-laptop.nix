41dcc68a68eadd597c25c0d8eb0e101320369c81
Backendt <matisse.beckandt@proton.me>
2024-09-26 03:05:07 +0200

    Enabled natural scrolling on laptop

--- modules/user/hyprland/hyprland-laptop.nix
+++ modules/user/hyprland/hyprland-laptop.nix
@@ -6,13 +6,16 @@
         ./common.nix
     ];
 
-    # Disable battery hungry effects
     wayland.windowManager.hyprland.settings = {
+        # Disable battery hungry effects
         decoration = {
             drop_shadow = false;
             blur.enabled = lib.mkForce false;
         };
         misc.vfr = true;
+
+        # Invert scrolling direction
+        input.natural_scroll = true;
     };
 
 }
