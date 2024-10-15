51aa8afd04d78312983b784ea63ce9f776a7b0ef
Backendt <matisse.beckandt@proton.me>
2024-10-15 21:16:32 +0200

    Added poimandres theme to Nixvim

--- modules/user/programming.nix
+++ modules/user/programming.nix
@@ -3,6 +3,7 @@
 {
     home.packages = with pkgs; [
         python3
+        python311Packages.requests
         jetbrains.idea-community
         jdk
     ];
--- modules/user/stylix/overrides/poimandres/nixvim.nix
+++ modules/user/stylix/overrides/poimandres/nixvim.nix
@@ -2,5 +2,8 @@
 
 {
     stylix.targets.nixvim.enable = false;
-    programs.nixvim.colorscheme.poimandres.enable = true;
+    programs.nixvim.colorschemes.poimandres = {
+		enable = true;
+		settings.disable_background = true;
+    };
 }
