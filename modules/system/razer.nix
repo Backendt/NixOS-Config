9582983e34bae5e04123937c27896eea98437ebb
Backendt <matisse.beckandt@proton.me>
2024-11-13 21:57:12 +0100

    Reduced razer battery notifications

--- modules/system/razer.nix
+++ modules/system/razer.nix
@@ -5,6 +5,10 @@
     hardware.openrazer = {
         enable = true;
         users = [ "mat" ];
+        batteryNotifier = {
+            percentage = 10; # Notify when below 10% battery
+            frequency = 30*60; # Notify every 30 minutes
+        };
     };
 
     # Add razergenie
