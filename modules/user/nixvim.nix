c8b3e44fd638f5992fce0e3695ca5cd8ffdcb856
Backendt <matisse.beckandt@proton.me>
2024-11-26 18:45:15 +0100

    Added nixvim keybinds and removed useless LSPs

--- modules/user/nixvim.nix
+++ modules/user/nixvim.nix
@@ -1,6 +1,7 @@
-{ config, inputs, pkgs, ... }:
+{ inputs, ... }:
 
 {
+    # TODO Replace Nixvim with Neovim
     imports = [ inputs.nixvim.homeManagerModules.nixvim ];
 
     programs.nixvim = {
@@ -8,32 +9,34 @@
         defaultEditor = true;
         vimAlias = true;
 
-        globals = {
-            mapleader = " ";
-        };
+        globals.mapleader = " ";
 
         plugins = {
             lsp = {
                 enable = true;
                 servers = {
-                    #eslint.enable = true; # JavaScript
                     dockerls.enable = true;
-                    #html.enable = true;
-                    #cssls.enable = true;
                     bashls.enable = true;
-                    #jsonls.enable = true;
                     nixd.enable = true; # Nix
                     pyright.enable = true; # Python
                     sqls.enable = true; # SQL
                     rust-analyzer = {
                         enable = true;
-                        installCargo = true;
-                        installRustc = true;
+                        installCargo = false;
+                        installRustc = false;
                     };
                 };
-                keymaps.diagnostic = {
-                    "<leader><Right>" = "goto_next";
-                    "<leader><Left>" = "goto_prev";
+                keymaps = {
+                    diagnostic = {
+                        "<leader><Right>" = "goto_next";
+                        "<leader><Left>" = "goto_prev";
+                    };
+                    lspBuf = {
+                        "gd" = "definition";
+                        "gD" = "references";
+                        "gt" = "type_definition";
+                        "gi" = "implementation";
+                    };
                 };
             };
             
@@ -54,6 +57,56 @@
                     { name = "path"; }
                     { name = "buffer"; }
                 ];
+                settings.mapping = { # From MikaelFangel's nixvim config
+                    "<C-n>" = "cmp.mapping.select_next_item()";
+                    "<C-p>" = "cmp.mapping.select_prev_item()";
+                    "<C-j>" = "cmp.mapping.select_next_item()";
+                    "<C-k>" = "cmp.mapping.select_prev_item()";
+                    "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                    "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                    "<C-Space>" = "cmp.mapping.complete()";
+                    "<S-Tab>" = "cmp.mapping.close()";
+                    "<Tab>" =
+                    # lua 
+                    ''
+                      function(fallback)
+                        local line = vim.api.nvim_get_current_line()
+                        if line:match("^%s*$") then
+                          fallback()
+                        elseif cmp.visible() then
+                          cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+                        else
+                          fallback()
+                        end
+                      end
+                    '';
+                    "<Down>" =
+                    # lua
+                    ''
+                      function(fallback)
+                        if cmp.visible() then
+                          cmp.select_next_item()
+                        elseif require("luasnip").expand_or_jumpable() then
+                          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                        else
+                          fallback()
+                        end
+                      end
+                    '';
+                    "<Up>" =
+                    # lua
+                    ''
+                      function(fallback)
+                        if cmp.visible() then
+                          cmp.select_prev_item()
+                        elseif require("luasnip").jumpable(-1) then
+                          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                        else
+                          fallback()
+                        end
+                      end
+                    '';
+                };
             };
 
             gitgutter.enable = true;
