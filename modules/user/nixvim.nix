5fbac21f1798b7a18eb8018f40c3a21a8a559a2e
Backendt <matisse.beckandt@proton.me>
2024-11-27 04:24:36 +0100

    Removed nixvim keybinds for uninstalled plugins

--- modules/user/nixvim.nix
+++ modules/user/nixvim.nix
@@ -86,8 +86,6 @@
                       function(fallback)
                         if cmp.visible() then
                           cmp.select_next_item()
-                        elseif require("luasnip").expand_or_jumpable() then
-                          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
                         else
                           fallback()
                         end
@@ -99,8 +97,6 @@
                       function(fallback)
                         if cmp.visible() then
                           cmp.select_prev_item()
-                        elseif require("luasnip").jumpable(-1) then
-                          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
                         else
                           fallback()
                         end
